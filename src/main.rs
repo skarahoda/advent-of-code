d3e5ef36cc9f4bb7e2c8bfa3bbedffe7b9961347
Sertaç Karahoda <skarahoda@sabanciuniv.edu>
2022-12-30 19:33:54 -0500

    :sparkles: Solve seventeenth puzzle

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,7 @@ use solver::{
     solver_2022_14,
     solver_2022_15,
     solver_2022_16,
+    solver_2022_17,
 };
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -74,6 +75,8 @@ enum Day {
     Day15,
     #[value(name="16")]
     Day16,
+    #[value(name="17")]
+    Day17,
 }
 
 /// Search for a pattern in a file and display the lines that contain it.
@@ -122,6 +125,7 @@ fn main() {
         (Year::Year2022, Day::Day14) => print_answers(solver_2022_14::solve()),
         (Year::Year2022, Day::Day15) => print_answers(solver_2022_15::solve()),
         (Year::Year2022, Day::Day16) => print_answers(solver_2022_16::solve()),
+        (Year::Year2022, Day::Day17) => print_answers(solver_2022_17::solve()),
         _ => panic!("Puzzle is not solved yet!")
     }
 }
--- src/solver/mod.rs
+++ src/solver/mod.rs
@@ -21,4 +21,5 @@ pub mod solver_2022_13;
 pub mod solver_2022_14;
 pub mod solver_2022_15;
 pub mod solver_2022_16;
+pub mod solver_2022_17;
 pub(in self) mod utils;
