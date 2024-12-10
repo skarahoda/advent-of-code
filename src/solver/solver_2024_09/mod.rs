55a65b2d77d7cd005422bbba7df0e4ddc982f49d
Sertaç Karahoda <sertackarahoda@gmail.com>
2024-12-09 22:00:55 -0500

    :zap: revamp defragmentation

--- src/solver/solver_2024_09/mod.rs
+++ src/solver/solver_2024_09/mod.rs
@@ -2,11 +2,19 @@ mod input;
 
 use super::Solver;
 use input::INPUT;
+use std::collections::HashMap;
 
 #[derive(PartialEq, Eq, Copy, Clone)]
 enum Space {
     Empty,
     File(usize),
+}
+
+#[derive(Clone)]
+struct File {
+    id: usize,
+    size: usize,
+    start: usize,
 }
 
 fn parse_input(input: &str) -> Vec<Space> {
@@ -25,15 +33,43 @@ fn parse_input(input: &str) -> Vec<Space> {
         .collect()
 }
 
+fn get_files_and_empty_spaces(input: &str) -> (Vec<File>, HashMap<usize, Vec<usize>>) {
+    let mut files = Vec::new();
+    let mut empty_spaces = HashMap::new();
+    let mut current_position = 0usize;
+    for (i, c) in input.chars().enumerate() {
+        let size = c.to_digit(10).unwrap() as usize;
+        if i % 2 == 0 {
+            files.push(File {
+                id: i / 2,
+                size,
+                start: current_position,
+            });
+        } else {
+            empty_spaces
+                .entry(size)
+                .or_insert(Vec::new())
+                .push(current_position);
+        };
+        current_position += size;
+    }
+    (files, empty_spaces)
+}
+
 #[derive(Clone)]
 pub struct Solver202409 {
     disk: Vec<Space>,
+    empty_spaces: HashMap<usize, Vec<usize>>,
+    files: Vec<File>,
 }
 
 impl From<&str> for Solver202409 {
     fn from(value: &str) -> Self {
+        let (files, empty_spaces) = get_files_and_empty_spaces(value);
         Self {
             disk: parse_input(value),
+            files,
+            empty_spaces,
         }
     }
 }
@@ -49,7 +85,7 @@ impl Solver202409 {
             self.disk.pop();
         }
     }
-    fn get_first_empty_space_index(&self, skip: usize) -> Option<usize> {
+    fn get_first_empty_space_index_in_disk(&self, skip: usize) -> Option<usize> {
         self.disk
             .iter()
             .skip(skip)
@@ -57,76 +93,64 @@ impl Solver202409 {
             .map(|pos| pos + skip)
     }
 
-    fn get_block_size(&self, start: usize) -> usize {
-        let space = self.disk[start];
-        self.disk
-            .iter()
-            .skip(start)
-            .position(|s| *s != space)
-            .unwrap_or_else(|| self.disk.len() - start)
-    }
-
-    fn get_block_info(&self, file_id: usize) -> (usize, usize) {
-        let start = self
-            .disk
-            .iter()
-            .position(|space| *space == Space::File(file_id))
-            .unwrap();
-        (start, self.get_block_size(start))
-    }
-
-    fn get_first_empty_space_block(&self, size: usize, max_index: usize) -> Option<usize> {
-        let mut empty_space_index = self.get_first_empty_space_index(0);
-        while let Some(i) = empty_space_index {
-            if i >= max_index {
-                return None;
-            }
-            let found_space = self.get_block_size(i);
-            if found_space >= size {
-                return Some(i);
-            }
-            empty_space_index = self.get_first_empty_space_index(i + found_space);
-        }
-        None
-    }
-
     fn defragmentation(&mut self) {
         self.remove_empty_spaces_at_end();
 
-        let mut empty_space_index = self.get_first_empty_space_index(0);
+        let mut empty_space_index = self.get_first_empty_space_index_in_disk(0);
         while let Some(i) = empty_space_index {
             self.disk[i] = self.disk.pop().unwrap();
             self.remove_empty_spaces_at_end();
-            empty_space_index = self.get_first_empty_space_index(i + 1);
+            empty_space_index = self.get_first_empty_space_index_in_disk(i + 1);
+        }
+    }
+
+    fn get_first_empty_space_index_for_file(&self, file: &File) -> Option<(usize, usize)> {
+        (file.size..10).fold(None, |acc, size| {
+            let location = self.empty_spaces.get(&size).and_then(|v| v.get(0));
+            if location.is_some_and(|&location| {
+                location < file.start && acc.is_none_or(|(_, acc_location)| location < acc_location)
+            }) {
+                Some((size, *location.unwrap()))
+            } else {
+                acc
+            }
+        })
+    }
+
+    fn insert_empty_spaces(&mut self, size: usize, location: usize) {
+        let vector = self.empty_spaces.entry(size).or_insert(Vec::new());
+        match vector.binary_search(&location) {
+            Ok(_) => {
+                panic!("Location already exists")
+            }
+            Err(index) => vector.insert(index, location),
         }
     }
 
     fn defragmentation_with_moving_whole_files(&mut self) {
-        self.remove_empty_spaces_at_end();
-        let max_file_id = self
-            .disk
-            .iter()
-            .rfind(|&&space| space != Space::Empty)
-            .map(|s| match s {
-                Space::File(i) => i,
-                _ => panic!("unreachable code"),
-            })
-            .unwrap();
-        for i in (0usize..*max_file_id + 1).rev() {
-            let (start, size) = self.get_block_info(i);
-            let empty_block = self.get_first_empty_space_block(size, start);
-            if let Some(empty_block) = empty_block {
-                for j in empty_block..empty_block + size {
-                    self.disk[j] = Space::File(i);
-                }
-                for j in start..start + size {
-                    self.disk[j] = Space::Empty;
+        let mut files = self.files.clone();
+        for file in files.iter_mut().rev() {
+            if let Some((size, location)) = self.get_first_empty_space_index_for_file(file) {
+                file.start = location;
+                self.empty_spaces.entry(size).and_modify(|v| {
+                    v.remove(0);
+                });
+                if size > file.size {
+                    self.insert_empty_spaces(size - file.size, location + file.size);
                 }
             }
-            self.remove_empty_spaces_at_end();
         }
+        self.files = files;
     }
-    fn checksum(&self) -> usize {
+
+    fn checksum_from_files(&self) -> usize {
+        self.files
+            .iter()
+            .flat_map(|file| (file.start..file.start + file.size).map(|i| (i, file.id)))
+            .fold(0, |acc, (id, location)| acc + (id * location))
+    }
+
+    fn checksum_from_disk(&self) -> usize {
         self.disk
             .iter()
             .enumerate()
@@ -141,12 +165,12 @@ impl Solver<usize, usize> for Solver202409 {
     fn solve_first_part(&self) -> usize {
         let mut cloned_self = self.clone();
         cloned_self.defragmentation();
-        cloned_self.checksum()
+        cloned_self.checksum_from_disk()
     }
     fn solve_second_part(&self) -> usize {
         let mut cloned_self = self.clone();
         cloned_self.defragmentation_with_moving_whole_files();
-        cloned_self.checksum()
+        cloned_self.checksum_from_files()
     }
 }
 
