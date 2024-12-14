20fe97c78296dba74acdee13c2fbf7fa61d0b4d3
Sertaç Karahoda <sertackarahoda@gmail.com>
2024-12-13 23:16:40 -0500

    :recycle: Use Solver trait in 2022/16

--- src/solver/solver_2022_16/mod.rs
+++ src/solver/solver_2022_16/mod.rs
@@ -1,3 +1,4 @@
+use super::Solver;
 mod input;
 use input::INPUT;
 use regex::Regex;
@@ -18,87 +19,6 @@ impl Valve {
 
 type Valves = HashMap<String, Valve>;
 
-fn get_shortest_paths(valves: &Valves, root: &str) -> HashMap<String, i32> {
-    let mut result: HashMap<String, i32> = HashMap::new();
-    let mut distances: HashMap<String, i32> = HashMap::new();
-    let mut queue: VecDeque<String> = VecDeque::new();
-    distances.insert(root.to_string(), 0);
-    queue.push_back(root.to_string());
-
-    while let Some(current_point) = queue.pop_front() {
-        let neighbours = &valves.get(&current_point).unwrap().tunnels;
-        let current_distance = *distances.get(&current_point).unwrap();
-        for neighbour in neighbours {
-            if !distances.contains_key(neighbour) {
-                distances.insert(neighbour.to_string(), current_distance + 1);
-                queue.push_back(neighbour.to_string());
-                if valves.get(neighbour).unwrap().flow > 0 {
-                    result.insert(neighbour.to_string(), current_distance + 1);
-                }
-            }
-        }
-    }
-    result
-}
-
-fn get_valves(input: &str) -> Valves {
-    let re =
-        Regex::new(r"Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? (.*)").unwrap();
-    input
-        .split("\n")
-        .map(|row| {
-            let captures = re.captures(row).unwrap();
-            (
-                captures.get(1).unwrap().as_str().to_string(),
-                Valve::new(
-                    captures.get(2).unwrap().as_str().parse().unwrap(),
-                    captures
-                        .get(3)
-                        .unwrap()
-                        .as_str()
-                        .split(", ")
-                        .map(|s| String::from(s))
-                        .collect(),
-                ),
-            )
-        })
-        .collect()
-}
-
-fn solve_first_part(valves: &Valves) -> i32 {
-    let mut candidates: Vec<(i32, i32, &str, HashSet<String>)> = Vec::new();
-    let shortest_path_map: HashMap<&str, HashMap<String, i32>> = valves
-        .keys()
-        .map(|point| (point.as_str(), get_shortest_paths(valves, point)))
-        .collect();
-    candidates.push((0, 30, "AA", HashSet::from(["AA".to_string()])));
-    let mut winner = 0;
-
-    while let Some((score, remaining_minutes, current_point, visited_points)) = candidates.pop() {
-        let shortest_paths = shortest_path_map.get(current_point).unwrap();
-        for (next_point, distance) in shortest_paths {
-            let remaining_minutes_after_open = remaining_minutes - distance - 1;
-            if !visited_points.contains(next_point) && remaining_minutes_after_open > 0 {
-                let score_for_valve =
-                    remaining_minutes_after_open * valves.get(next_point).unwrap().flow;
-                let mut new_visited_points = visited_points.clone();
-                new_visited_points.insert(next_point.to_string());
-                let next_score = score + score_for_valve;
-                candidates.push((
-                    score + score_for_valve,
-                    remaining_minutes_after_open,
-                    next_point,
-                    new_visited_points,
-                ));
-                if next_score > winner {
-                    winner = next_score;
-                }
-            }
-        }
-    }
-    winner
-}
-
 fn get_max_possible_remaining_score(
     remaining_times: (i32, i32),
     mut remaining_flows: Vec<i32>,
@@ -115,54 +35,99 @@ fn get_max_possible_remaining_score(
     }
 }
 
-fn solve_second_part(valves: &Valves) -> i32 {
-    let mut candidates: Vec<(i32, (i32, &str), (i32, &str), HashSet<String>)> = Vec::new();
-    let shortest_path_map: HashMap<&str, HashMap<String, i32>> = valves
-        .keys()
-        .map(|point| (point.as_str(), get_shortest_paths(valves, point)))
-        .collect();
-    let mut sorted_flows: Vec<(&str, i32)> = valves
-        .iter()
-        .filter(|&(_, valve)| valve.flow > 0)
-        .map(|(name, valve)| (name.as_str(), valve.flow))
-        .collect();
-    sorted_flows.sort();
-    candidates.push((0, (26, "AA"), (26, "AA"), HashSet::from(["AA".to_string()])));
-    let mut winner = 0;
+pub struct Solver2022_16 {
+    valves: Valves,
+}
 
-    while let Some((score, player_state, elephant_state, visited_points)) = candidates.pop() {
-        for (remaining_minutes, current_point, other_state) in [
-            (player_state.0, player_state.1, elephant_state),
-            (elephant_state.0, elephant_state.1, player_state),
-        ] {
+impl Default for Solver2022_16 {
+    fn default() -> Self {
+        Self::from(INPUT)
+    }
+}
+
+impl<'a> From<&'a str> for Solver2022_16 {
+    fn from(input: &'a str) -> Self {
+        let re = Regex::new(r"Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? (.*)")
+            .unwrap();
+
+        Self {
+            valves: input
+                .split("\n")
+                .map(|row| {
+                    let captures = re.captures(row).unwrap();
+                    (
+                        captures.get(1).unwrap().as_str().to_string(),
+                        Valve::new(
+                            captures.get(2).unwrap().as_str().parse().unwrap(),
+                            captures
+                                .get(3)
+                                .unwrap()
+                                .as_str()
+                                .split(", ")
+                                .map(|s| String::from(s))
+                                .collect(),
+                        ),
+                    )
+                })
+                .collect(),
+        }
+    }
+}
+
+impl Solver2022_16 {
+    fn get_shortest_paths(&self, root: &str) -> HashMap<String, i32> {
+        let mut result: HashMap<String, i32> = HashMap::new();
+        let mut distances: HashMap<String, i32> = HashMap::new();
+        let mut queue: VecDeque<String> = VecDeque::new();
+        distances.insert(root.to_string(), 0);
+        queue.push_back(root.to_string());
+
+        while let Some(current_point) = queue.pop_front() {
+            let neighbours = &self.valves.get(&current_point).unwrap().tunnels;
+            let current_distance = *distances.get(&current_point).unwrap();
+            for neighbour in neighbours {
+                if !distances.contains_key(neighbour) {
+                    distances.insert(neighbour.to_string(), current_distance + 1);
+                    queue.push_back(neighbour.to_string());
+                    if self.valves.get(neighbour).unwrap().flow > 0 {
+                        result.insert(neighbour.to_string(), current_distance + 1);
+                    }
+                }
+            }
+        }
+        result
+    }
+
+    fn get_shortest_paths_map(&self) -> HashMap<&str, HashMap<String, i32>> {
+        self.valves
+            .keys()
+            .map(|point| (point.as_str(), self.get_shortest_paths(point)))
+            .collect()
+    }
+}
+
+impl Solver<i32, i32> for Solver2022_16 {
+    fn solve_first_part(&self) -> i32 {
+        let mut candidates: Vec<(i32, i32, &str, HashSet<String>)> = Vec::new();
+        let shortest_path_map = self.get_shortest_paths_map();
+        candidates.push((0, 30, "AA", HashSet::from(["AA".to_string()])));
+        let mut winner = 0;
+
+        while let Some((score, remaining_minutes, current_point, visited_points)) = candidates.pop()
+        {
             let shortest_paths = shortest_path_map.get(current_point).unwrap();
             for (next_point, distance) in shortest_paths {
                 let remaining_minutes_after_open = remaining_minutes - distance - 1;
-                let score_for_valve =
-                    remaining_minutes_after_open * valves.get(next_point).unwrap().flow;
-                let next_score = score + score_for_valve;
-                let remaining_flows: Vec<i32> = sorted_flows
-                    .iter()
-                    .filter(|(valve_name, _)| {
-                        valve_name != next_point && !visited_points.contains(*valve_name)
-                    })
-                    .map(|&(_, flow)| flow)
-                    .collect();
-                let max_possible_score = next_score
-                    + get_max_possible_remaining_score(
-                    (remaining_minutes_after_open, other_state.0),
-                    remaining_flows,
-                );
-                if max_possible_score > winner
-                    && !visited_points.contains(next_point)
-                    && remaining_minutes_after_open > 0
-                {
+                if !visited_points.contains(next_point) && remaining_minutes_after_open > 0 {
+                    let score_for_valve =
+                        remaining_minutes_after_open * self.valves.get(next_point).unwrap().flow;
                     let mut new_visited_points = visited_points.clone();
                     new_visited_points.insert(next_point.to_string());
+                    let next_score = score + score_for_valve;
                     candidates.push((
-                        next_score,
-                        (remaining_minutes_after_open, next_point),
-                        other_state,
+                        score + score_for_valve,
+                        remaining_minutes_after_open,
+                        next_point,
                         new_visited_points,
                     ));
                     if next_score > winner {
@@ -171,13 +136,66 @@ fn solve_second_part(valves: &Valves) -> i32 {
                 }
             }
         }
+        winner
     }
-    winner
-}
 
-pub fn solve() -> (i32, i32) {
-    let valves = get_valves(INPUT);
-    (solve_first_part(&valves), solve_second_part(&valves))
+    fn solve_second_part(&self) -> i32 {
+        let mut candidates: Vec<(i32, (i32, &str), (i32, &str), HashSet<String>)> = Vec::new();
+        let shortest_path_map = self.get_shortest_paths_map();
+        let mut sorted_flows: Vec<(&str, i32)> = self
+            .valves
+            .iter()
+            .filter(|&(_, valve)| valve.flow > 0)
+            .map(|(name, valve)| (name.as_str(), valve.flow))
+            .collect();
+        sorted_flows.sort();
+        candidates.push((0, (26, "AA"), (26, "AA"), HashSet::from(["AA".to_string()])));
+        let mut winner = 0;
+
+        while let Some((score, player_state, elephant_state, visited_points)) = candidates.pop() {
+            for (remaining_minutes, current_point, other_state) in [
+                (player_state.0, player_state.1, elephant_state),
+                (elephant_state.0, elephant_state.1, player_state),
+            ] {
+                let shortest_paths = shortest_path_map.get(current_point).unwrap();
+                for (next_point, distance) in shortest_paths {
+                    let remaining_minutes_after_open = remaining_minutes - distance - 1;
+                    let score_for_valve =
+                        remaining_minutes_after_open * self.valves.get(next_point).unwrap().flow;
+                    let next_score = score + score_for_valve;
+                    let remaining_flows: Vec<i32> = sorted_flows
+                        .iter()
+                        .filter(|(valve_name, _)| {
+                            valve_name != next_point && !visited_points.contains(*valve_name)
+                        })
+                        .map(|&(_, flow)| flow)
+                        .collect();
+                    let max_possible_score = next_score
+                        + get_max_possible_remaining_score(
+                            (remaining_minutes_after_open, other_state.0),
+                            remaining_flows,
+                        );
+                    if max_possible_score > winner
+                        && !visited_points.contains(next_point)
+                        && remaining_minutes_after_open > 0
+                    {
+                        let mut new_visited_points = visited_points.clone();
+                        new_visited_points.insert(next_point.to_string());
+                        candidates.push((
+                            next_score,
+                            (remaining_minutes_after_open, next_point),
+                            other_state,
+                            new_visited_points,
+                        ));
+                        if next_score > winner {
+                            winner = next_score;
+                        }
+                    }
+                }
+            }
+        }
+        winner
+    }
 }
 
 #[cfg(test)]
@@ -197,69 +215,14 @@ mod tests {
         Valve JJ has flow rate=21; tunnel leads to valve II";
 
     #[test]
-    fn should_get_valves() {
-        assert_eq!(
-            get_valves(EXAMPLE),
-            HashMap::from([
-                (
-                    "AA".to_string(),
-                    Valve::new(
-                        0,
-                        HashSet::from(["DD".to_string(), "II".to_string(), "BB".to_string(), ])
-                    )
-                ),
-                (
-                    "BB".to_string(),
-                    Valve::new(13, HashSet::from(["CC".to_string(), "AA".to_string()]))
-                ),
-                (
-                    "CC".to_string(),
-                    Valve::new(2, HashSet::from(["DD".to_string(), "BB".to_string()]))
-                ),
-                (
-                    "DD".to_string(),
-                    Valve::new(
-                        20,
-                        HashSet::from(["CC".to_string(), "AA".to_string(), "EE".to_string()])
-                    )
-                ),
-                (
-                    "EE".to_string(),
-                    Valve::new(3, HashSet::from(["FF".to_string(), "DD".to_string()]))
-                ),
-                (
-                    "FF".to_string(),
-                    Valve::new(0, HashSet::from(["EE".to_string(), "GG".to_string()]))
-                ),
-                (
-                    "GG".to_string(),
-                    Valve::new(0, HashSet::from(["FF".to_string(), "HH".to_string()]))
-                ),
-                (
-                    "HH".to_string(),
-                    Valve::new(22, HashSet::from(["GG".to_string()]))
-                ),
-                (
-                    "II".to_string(),
-                    Valve::new(0, HashSet::from(["AA".to_string(), "JJ".to_string()]))
-                ),
-                (
-                    "JJ".to_string(),
-                    Valve::new(21, HashSet::from(["II".to_string()]))
-                ),
-            ])
-        );
-    }
-
-    #[test]
     fn should_solve_first_part() {
-        let valves = get_valves(EXAMPLE);
-        assert_eq!(solve_first_part(&valves), 1651);
+        let solver = Solver2022_16::from(EXAMPLE);
+        assert_eq!(solver.solve_first_part(), 1651);
     }
 
     #[test]
     fn should_solve_second_part() {
-        let valves = get_valves(EXAMPLE);
-        assert_eq!(solve_second_part(&valves), 1707);
+        let solver = Solver2022_16::from(EXAMPLE);
+        assert_eq!(solver.solve_second_part(), 1707);
     }
 }
