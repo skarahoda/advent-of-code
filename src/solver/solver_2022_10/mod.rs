77a46a4dcf43ad4a19b4a0c663549fb27a4f9720
Sertaç Karahoda <sertackarahoda@gmail.com>
2024-12-16 21:41:24 -0500

    :recycle: Use Solver trait in 2022/10

--- src/solver/solver_2022_10/mod.rs
+++ src/solver/solver_2022_10/mod.rs
@@ -1,3 +1,4 @@
+use super::Solver;
 use regex::Regex;
 
 #[derive(Clone)]
@@ -6,45 +7,29 @@ enum Operation {
     Add(i32),
 }
 
-fn get_operations() -> Vec<Operation> {
-    let re = Regex::new(r"addx (-?\d+)").unwrap();
-
-    include_str!("input.txt")
-        .split("\n")
-        .map(|operation| match operation {
-            "noop" => Operation::NoOp,
-            other => Operation::Add(re.captures(other).unwrap()[1].parse().unwrap()),
-        })
-        .collect()
+pub struct Solver2022_10 {
+    operations: Vec<Operation>,
 }
 
-fn solve_first_part(operations: &Vec<Operation>) -> i32 {
-    let mut cycle_count = 0;
-    let mut register = 1;
-    let mut result = 0;
-    for operation in operations {
-        match operation {
-            Operation::NoOp => {
-                cycle_count += 1;
-                match cycle_count % 40 {
-                    20 => {
-                        result += cycle_count * register;
-                    }
-                    _ => {}
-                }
-            }
-            Operation::Add(value) => {
-                cycle_count += 2;
-                match cycle_count % 40 {
-                    20 => result += cycle_count * register,
-                    21 => result += (cycle_count - 1) * register,
-                    _ => {}
-                }
-                register += value;
-            }
-        }
+impl Default for Solver2022_10 {
+    fn default() -> Self {
+        Self::from(include_str!("input.txt"))
     }
-    result
+}
+
+impl From<&str> for Solver2022_10 {
+    fn from(input: &str) -> Self {
+        let re = Regex::new(r"addx (-?\d+)").unwrap();
+
+        let operations = input
+            .split("\n")
+            .map(|operation| match operation {
+                "noop" => Operation::NoOp,
+                other => Operation::Add(re.captures(other).unwrap()[1].parse().unwrap()),
+            })
+            .collect();
+        Self { operations }
+    }
 }
 
 fn get_pixel(cycle_count: i32, register: i32) -> String {
@@ -57,206 +42,232 @@ fn get_pixel(cycle_count: i32, register: i32) -> String {
     }
 }
 
-fn solve_second_part(operations: &Vec<Operation>) -> String {
-    let mut cycle_count = 0;
-    let mut register = 0;
-    let mut result = String::from("");
-    for operation in operations {
-        match operation {
-            Operation::NoOp => {
-                result += get_pixel(cycle_count, register).as_str();
-                cycle_count += 1;
-            }
-            Operation::Add(value) => {
-                result += get_pixel(cycle_count, register).as_str();
-                cycle_count += 1;
-
-                result += get_pixel(cycle_count, register).as_str();
-                cycle_count += 1;
-
-                register += value;
+impl Solver<i32, String> for Solver2022_10 {
+    fn solve_first_part(&self) -> i32 {
+        let mut cycle_count = 0;
+        let mut register = 1;
+        let mut result = 0;
+        for operation in &self.operations {
+            match operation {
+                Operation::NoOp => {
+                    cycle_count += 1;
+                    match cycle_count % 40 {
+                        20 => {
+                            result += cycle_count * register;
+                        }
+                        _ => {}
+                    }
+                }
+                Operation::Add(value) => {
+                    cycle_count += 2;
+                    match cycle_count % 40 {
+                        20 => result += cycle_count * register,
+                        21 => result += (cycle_count - 1) * register,
+                        _ => {}
+                    }
+                    register += value;
+                }
             }
         }
+        result
     }
-    result
-}
 
-pub fn solve() -> (i32, String) {
-    let operations = get_operations();
-    (
-        solve_first_part(&operations),
-        solve_second_part(&operations),
-    )
+    fn solve_second_part(&self) -> String {
+        let mut cycle_count = 0;
+        let mut register = 0;
+        let mut result = String::from("");
+        for operation in &self.operations {
+            match operation {
+                Operation::NoOp => {
+                    result += get_pixel(cycle_count, register).as_str();
+                    cycle_count += 1;
+                }
+                Operation::Add(value) => {
+                    result += get_pixel(cycle_count, register).as_str();
+                    cycle_count += 1;
+
+                    result += get_pixel(cycle_count, register).as_str();
+                    cycle_count += 1;
+
+                    register += value;
+                }
+            }
+        }
+        result
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::Operation;
+    use super::*;
 
-    static EXAMPLE: [Operation; 146] = [
-        Operation::Add(15),
-        Operation::Add(-11),
-        Operation::Add(6),
-        Operation::Add(-3),
-        Operation::Add(5),
-        Operation::Add(-1),
-        Operation::Add(-8),
-        Operation::Add(13),
-        Operation::Add(4),
-        Operation::NoOp,
-        Operation::Add(-1),
-        Operation::Add(5),
-        Operation::Add(-1),
-        Operation::Add(5),
-        Operation::Add(-1),
-        Operation::Add(5),
-        Operation::Add(-1),
-        Operation::Add(5),
-        Operation::Add(-1),
-        Operation::Add(-35),
-        Operation::Add(1),
-        Operation::Add(24),
-        Operation::Add(-19),
-        Operation::Add(1),
-        Operation::Add(16),
-        Operation::Add(-11),
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::Add(21),
-        Operation::Add(-15),
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::Add(-3),
-        Operation::Add(9),
-        Operation::Add(1),
-        Operation::Add(-3),
-        Operation::Add(8),
-        Operation::Add(1),
-        Operation::Add(5),
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::Add(-36),
-        Operation::NoOp,
-        Operation::Add(1),
-        Operation::Add(7),
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::Add(2),
-        Operation::Add(6),
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::Add(1),
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::Add(7),
-        Operation::Add(1),
-        Operation::NoOp,
-        Operation::Add(-13),
-        Operation::Add(13),
-        Operation::Add(7),
-        Operation::NoOp,
-        Operation::Add(1),
-        Operation::Add(-33),
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::Add(2),
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::Add(8),
-        Operation::NoOp,
-        Operation::Add(-1),
-        Operation::Add(2),
-        Operation::Add(1),
-        Operation::NoOp,
-        Operation::Add(17),
-        Operation::Add(-9),
-        Operation::Add(1),
-        Operation::Add(1),
-        Operation::Add(-3),
-        Operation::Add(11),
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::Add(1),
-        Operation::NoOp,
-        Operation::Add(1),
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::Add(-13),
-        Operation::Add(-19),
-        Operation::Add(1),
-        Operation::Add(3),
-        Operation::Add(26),
-        Operation::Add(-30),
-        Operation::Add(12),
-        Operation::Add(-1),
-        Operation::Add(3),
-        Operation::Add(1),
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::Add(-9),
-        Operation::Add(18),
-        Operation::Add(1),
-        Operation::Add(2),
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::Add(9),
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::Add(-1),
-        Operation::Add(2),
-        Operation::Add(-37),
-        Operation::Add(1),
-        Operation::Add(3),
-        Operation::NoOp,
-        Operation::Add(15),
-        Operation::Add(-21),
-        Operation::Add(22),
-        Operation::Add(-6),
-        Operation::Add(1),
-        Operation::NoOp,
-        Operation::Add(2),
-        Operation::Add(1),
-        Operation::NoOp,
-        Operation::Add(-10),
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::Add(20),
-        Operation::Add(1),
-        Operation::Add(2),
-        Operation::Add(2),
-        Operation::Add(-6),
-        Operation::Add(-11),
-        Operation::NoOp,
-        Operation::NoOp,
-        Operation::NoOp,
-    ];
+    static EXAMPLE: &str = "\
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop";
 
     #[test]
     fn should_solve_first_part_example() {
-        assert_eq!(super::solve_first_part(&EXAMPLE.to_vec()), 13140);
+        let solver = Solver2022_10::from(EXAMPLE);
+        assert_eq!(solver.solve_first_part(), 13140);
     }
 
     #[test]
     fn should_solve_second_part_example() {
+        let solver = Solver2022_10::from(EXAMPLE);
         assert_eq!(
-            super::solve_second_part(&EXAMPLE.to_vec()),
-            "##..##..##..##..##..##..##..##..##..##..\n\
-                    ###...###...###...###...###...###...###.\n\
-                    ####....####....####....####....####....\n\
-                    #####.....#####.....#####.....#####.....\n\
-                    ######......######......######......####\n\
-                    #######.......#######.......#######.....\n"
+            solver.solve_second_part(),
+            "\
+##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+"
         );
     }
 }
