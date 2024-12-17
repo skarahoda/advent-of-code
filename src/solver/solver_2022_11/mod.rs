658f8128040674d6392982d81c290e6a4a190e6d
Sertaç Karahoda <sertackarahoda@gmail.com>
2024-12-16 21:27:31 -0500

    :recycle: Use Solver trait in 2022/11

--- src/solver/solver_2022_11/mod.rs
+++ src/solver/solver_2022_11/mod.rs
@@ -1,3 +1,4 @@
+use super::Solver;
 use pest::iterators::Pair;
 use pest::Parser;
 use std::collections::HashMap;
@@ -36,7 +37,7 @@ impl Operation {
     }
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 struct Monkey {
     items: Vec<u64>,
     operation: Operation,
@@ -76,105 +77,116 @@ impl Monkey {
     }
 }
 
-fn get_monkeys(input: &str) -> HashMap<usize, Monkey> {
-    let program = SantaParser::parse(Rule::Program, input).unwrap_or_else(|e| panic!("{}", e));
-    let monkey_pairs = program.peek().unwrap().into_inner();
-    let mut result = HashMap::new();
-    for monkey_pair in monkey_pairs {
-        let pairs: Vec<Pair<Rule>> = monkey_pair.into_inner().collect();
-        let monkey_index: usize = pairs[0].as_str().parse().unwrap();
-        let items: Vec<u64> = pairs[1]
-            .to_owned()
-            .into_inner()
-            .map(|i| i.as_str().parse().unwrap())
-            .collect();
-        let left_operand = match pairs[2].as_rule() {
-            Rule::Old => Operand::Old,
-            Rule::Number => Operand::Number(pairs[2].as_str().parse().unwrap()),
-            other => panic!("syntax error: operand expected, found {:?}", other),
-        };
-        let right_operand = match pairs[4].as_rule() {
-            Rule::Old => Operand::Old,
-            Rule::Number => Operand::Number(pairs[4].as_str().parse().unwrap()),
-            other => panic!("syntax error: operand expected, found {:?}", other),
-        };
-        let operation = match pairs[3].as_rule() {
-            Rule::Add => Operation::Add(left_operand, right_operand),
-            Rule::Multiply => Operation::Multiply(left_operand, right_operand),
-            other => panic!("syntax error: operator expected, found {:?}", other),
-        };
-        let tester: u64 = pairs[5].as_str().parse().unwrap();
-        let if_true: usize = pairs[6].as_str().parse().unwrap();
-        let if_false: usize = pairs[7].as_str().parse().unwrap();
-        result.insert(
-            monkey_index,
-            Monkey::new(items, operation, tester, if_true, if_false),
-        );
-    }
-    result
+pub struct Solver2022_11 {
+    monkeys: HashMap<usize, Monkey>,
 }
 
-fn solve_part<F>(monkeys: &mut HashMap<usize, Monkey>, rounds: usize, relief: &F) -> u64
-where
-    F: Fn(u64) -> u64,
-{
-    let mut investigation_map: HashMap<usize, u64> = HashMap::new();
-    for _ in 0..rounds {
-        let mut new_items_map: HashMap<usize, Vec<u64>> = HashMap::new();
-        for (index, monkey) in monkeys.iter() {
-            for item in monkey.items.clone() {
-                let mut current_index = *index;
-                *investigation_map.entry(current_index).or_insert(0) += 1;
-                let (mut next_index, mut item) = monkey.throw(&item, relief).clone();
-                while current_index <= next_index {
-                    current_index = next_index;
+impl Default for Solver2022_11 {
+    fn default() -> Self {
+        Solver2022_11::from(include_str!("input.txt"))
+    }
+}
+
+impl From<&str> for Solver2022_11 {
+    fn from(input: &str) -> Self {
+        let program = SantaParser::parse(Rule::Program, input).unwrap_or_else(|e| panic!("{}", e));
+        let monkey_pairs = program.peek().unwrap().into_inner();
+        let mut monkeys = HashMap::new();
+
+        for monkey_pair in monkey_pairs {
+            let pairs: Vec<Pair<Rule>> = monkey_pair.into_inner().collect();
+            let monkey_index: usize = pairs[0].as_str().parse().unwrap();
+            let items: Vec<u64> = pairs[1]
+                .to_owned()
+                .into_inner()
+                .map(|i| i.as_str().parse().unwrap())
+                .collect();
+            let left_operand = match pairs[2].as_rule() {
+                Rule::Old => Operand::Old,
+                Rule::Number => Operand::Number(pairs[2].as_str().parse().unwrap()),
+                other => panic!("syntax error: operand expected, found {:?}", other),
+            };
+            let right_operand = match pairs[4].as_rule() {
+                Rule::Old => Operand::Old,
+                Rule::Number => Operand::Number(pairs[4].as_str().parse().unwrap()),
+                other => panic!("syntax error: operand expected, found {:?}", other),
+            };
+            let operation = match pairs[3].as_rule() {
+                Rule::Add => Operation::Add(left_operand, right_operand),
+                Rule::Multiply => Operation::Multiply(left_operand, right_operand),
+                other => panic!("syntax error: operator expected, found {:?}", other),
+            };
+            let tester: u64 = pairs[5].as_str().parse().unwrap();
+            let if_true: usize = pairs[6].as_str().parse().unwrap();
+            let if_false: usize = pairs[7].as_str().parse().unwrap();
+            monkeys.insert(
+                monkey_index,
+                Monkey::new(items, operation, tester, if_true, if_false),
+            );
+        }
+
+        Self { monkeys }
+    }
+}
+
+impl Solver2022_11 {
+    fn solve_part<F>(&self, rounds: usize, relief: F) -> u64
+    where
+        F: Fn(u64) -> u64,
+    {
+        let mut monkeys = self.monkeys.clone();
+        let mut investigation_map: HashMap<usize, u64> = HashMap::new();
+
+        for _ in 0..rounds {
+            let mut new_items_map: HashMap<usize, Vec<u64>> = HashMap::new();
+            for (index, monkey) in monkeys.iter() {
+                for item in monkey.items.clone() {
+                    let mut current_index = *index;
                     *investigation_map.entry(current_index).or_insert(0) += 1;
-                    (next_index, item) = monkeys
-                        .get(&current_index)
-                        .unwrap()
-                        .throw(&item, relief)
-                        .clone();
+                    let (mut next_index, mut item) = monkey.throw(&item, &relief).clone();
+                    while current_index <= next_index {
+                        current_index = next_index;
+                        *investigation_map.entry(current_index).or_insert(0) += 1;
+                        (next_index, item) = monkeys
+                            .get(&current_index)
+                            .unwrap()
+                            .throw(&item, &relief)
+                            .clone();
+                    }
+                    new_items_map
+                        .entry(next_index)
+                        .or_insert(Vec::new())
+                        .push(item);
                 }
-                new_items_map
-                    .entry(next_index)
-                    .or_insert(Vec::new())
-                    .push(item);
+            }
+            for monkey in monkeys.values_mut() {
+                monkey.items.clear();
+            }
+            for (index, new_items) in new_items_map {
+                for item in new_items {
+                    monkeys.get_mut(&index).unwrap().items.push(item);
+                }
             }
         }
-        for monkey in monkeys.values_mut() {
-            monkey.items.clear();
-        }
-        for (index, new_items) in new_items_map {
-            for item in new_items {
-                monkeys.get_mut(&index).unwrap().items.push(item);
-            }
-        }
+
+        let mut investigations: Vec<&u64> = investigation_map.values().collect();
+        investigations.sort();
+        investigations.pop().unwrap() * investigations.pop().unwrap()
     }
-    let mut investigations: Vec<&u64> = investigation_map.values().collect();
-    investigations.sort();
-    let most_investigation = investigations.pop().unwrap();
-    let second_most_investigation = investigations.pop().unwrap();
-    most_investigation * second_most_investigation
 }
 
-fn solve_first_part(input: &str) -> u64 {
-    let mut monkeys = get_monkeys(&input);
-    solve_part(&mut monkeys, 20, &|value| value / 3)
-}
+impl Solver<u64, u64> for Solver2022_11 {
+    fn solve_first_part(&self) -> u64 {
+        self.solve_part(20, |x| x / 3)
+    }
 
-fn solve_second_part(input: &str) -> u64 {
-    let mut monkeys = get_monkeys(&input);
-    let relief = monkeys
-        .values()
-        .fold(1, |product, monkey| product * monkey.tester);
-    solve_part(&mut monkeys, 10000, &|value| value % relief)
-}
-
-pub fn solve() -> (u64, u64) {
-    (
-        solve_first_part(include_str!("input.txt")),
-        solve_second_part(include_str!("input.txt")),
-    )
+    fn solve_second_part(&self) -> u64 {
+        let relief = self
+            .monkeys
+            .values()
+            .fold(1, |product, monkey| product * monkey.tester);
+        self.solve_part(10000, move |x| x % relief)
+    }
 }
 
 #[cfg(test)]
@@ -212,8 +224,9 @@ mod tests {
 
     #[test]
     fn should_parse_input() {
+        let solver = Solver2022_11::from(EXAMPLE);
         assert_eq!(
-            get_monkeys(EXAMPLE),
+            solver.monkeys,
             HashMap::<usize, Monkey>::from([
                 (
                     0,
@@ -261,20 +274,12 @@ mod tests {
 
     #[test]
     fn should_solve_first_part_example() {
-        assert_eq!(
-            solve_part(&mut get_monkeys(EXAMPLE), 20, &|value| value / 3),
-            10605
-        );
+        let solver = Solver2022_11::from(EXAMPLE);
+        assert_eq!(solver.solve_first_part(), 10605);
     }
     #[test]
     fn should_solve_second_part_example() {
-        let mut monkeys = get_monkeys(EXAMPLE);
-        let relief = monkeys
-            .values()
-            .fold(1, |product, monkey| product * monkey.tester);
-        assert_eq!(
-            solve_part(&mut monkeys, 10000, &|value| value % relief),
-            2713310158
-        );
+        let solver = Solver2022_11::from(EXAMPLE);
+        assert_eq!(solver.solve_second_part(), 2713310158);
     }
 }
