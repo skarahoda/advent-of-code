69261b21e4f77a0f8df28cada68762e71e283aea
Sertaç Karahoda <sertackarahoda@gmail.com>
2024-12-19 18:26:02 -0500

    :recycle: Use Solver trait in 2022/06

--- src/solver/solver_2022_06/mod.rs
+++ src/solver/solver_2022_06/mod.rs
@@ -1,104 +1,103 @@
+use crate::solver::Solver;
+use std::borrow::Cow;
 use std::collections::HashSet;
 
-fn find_start_of_message_marker(input: &str, message_length: usize) -> usize {
-    let chars: Vec<char> = input.chars().collect();
-    for i in message_length - 1..chars.len() {
-        let mut set = HashSet::<char>::new();
-        for j in 0..message_length {
-            set.insert(chars[i - j]);
-        }
-        if set.len() == message_length {
-            return i + 1;
+pub struct Solver2022_06<'a> {
+    input: Cow<'a, str>,
+}
+
+impl<'a> Default for Solver2022_06<'a> {
+    fn default() -> Self {
+        Self::from(include_str!("input.txt"))
+    }
+}
+
+impl<'a> From<&'a str> for Solver2022_06<'a> {
+    fn from(input: &'a str) -> Self {
+        Self {
+            input: Cow::Borrowed(input),
         }
     }
-    panic!("couldn't solve it.");
 }
 
-fn solve_first_part(input: &str) -> usize {
-    find_start_of_message_marker(input, 4)
+impl<'a> Solver2022_06<'a> {
+    fn find_start_of_message_marker(&self, message_length: usize) -> usize {
+        let chars: Vec<char> = self.input.chars().collect();
+        for i in message_length - 1..chars.len() {
+            let mut set = HashSet::<char>::new();
+            for j in 0..message_length {
+                set.insert(chars[i - j]);
+            }
+            if set.len() == message_length {
+                return i + 1;
+            }
+        }
+        panic!("couldn't solve it.");
+    }
 }
 
-fn solve_second_part(input: &str) -> usize {
-    find_start_of_message_marker(input, 14)
-}
+impl<'a> Solver<usize, usize> for Solver2022_06<'a> {
+    fn solve_first_part(&self) -> usize {
+        self.find_start_of_message_marker(4)
+    }
 
-pub fn solve() -> (usize, usize) {
-    (
-        solve_first_part(include_str!("input.txt")),
-        solve_second_part(include_str!("input.txt")),
-    )
+    fn solve_second_part(&self) -> usize {
+        self.find_start_of_message_marker(14)
+    }
 }
 
 #[cfg(test)]
 mod find_start_of_message_marker {
+    use crate::solver::Solver;
+
     #[test]
     fn first_example() {
-        assert_eq!(
-            super::find_start_of_message_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4),
-            7
-        );
+        let solver = super::Solver2022_06::from("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+        assert_eq!(solver.solve_first_part(), 7);
     }
     #[test]
     fn second_example() {
-        assert_eq!(
-            super::find_start_of_message_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 4),
-            5
-        );
+        let solver = super::Solver2022_06::from("bvwbjplbgvbhsrlpgdmjqwftvncz");
+        assert_eq!(solver.solve_first_part(), 5);
     }
     #[test]
     fn third_example() {
-        assert_eq!(
-            super::find_start_of_message_marker("nppdvjthqldpwncqszvftbrmjlhg", 4),
-            6
-        );
+        let solver = super::Solver2022_06::from("nppdvjthqldpwncqszvftbrmjlhg");
+        assert_eq!(solver.solve_first_part(), 6);
     }
     #[test]
     fn fourth_example() {
-        assert_eq!(
-            super::find_start_of_message_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4),
-            10
-        );
+        let solver = super::Solver2022_06::from("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg");
+        assert_eq!(solver.solve_first_part(), 10);
     }
     #[test]
     fn fifth_example() {
-        assert_eq!(
-            super::find_start_of_message_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4),
-            11
-        );
+        let solver = super::Solver2022_06::from("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
+        assert_eq!(solver.solve_first_part(), 11);
     }
     #[test]
     fn first_example_in_second_part() {
-        assert_eq!(
-            super::find_start_of_message_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14),
-            19
-        );
+        let solver = super::Solver2022_06::from("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+        assert_eq!(solver.solve_second_part(), 19);
     }
     #[test]
     fn second_example_in_second_part() {
-        assert_eq!(
-            super::find_start_of_message_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 14),
-            23
-        );
+        let solver = super::Solver2022_06::from("bvwbjplbgvbhsrlpgdmjqwftvncz");
+        assert_eq!(solver.solve_second_part(), 23);
     }
     #[test]
     fn third_example_in_second_part() {
-        assert_eq!(
-            super::find_start_of_message_marker("nppdvjthqldpwncqszvftbrmjlhg", 14),
-            23
-        );
+        let solver = super::Solver2022_06::from("nppdvjthqldpwncqszvftbrmjlhg");
+        assert_eq!(solver.solve_second_part(), 23);
     }
     #[test]
     fn fourth_example_in_second_part() {
-        assert_eq!(
-            super::find_start_of_message_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14),
-            29
-        );
+        let solver = super::Solver2022_06::from("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg");
+        assert_eq!(solver.solve_second_part(), 29);
     }
     #[test]
     fn fifth_example_in_second_part() {
-        assert_eq!(
-            super::find_start_of_message_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14),
-            26
-        );
+        let solver = super::Solver2022_06::from("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
+        assert_eq!(solver.solve_second_part(), 26);
     }
 }
