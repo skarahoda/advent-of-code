32e5c89836435572b8daf2efe1e2dbc218f010d3
Sertaç Karahoda <sertackarahoda@gmail.com>
2024-12-26 10:32:52 -0500

    :sparkles: Solve 2024/24 part 2

--- src/solver/solver_2024_24/mod.rs
+++ src/solver/solver_2024_24/mod.rs
@@ -2,6 +2,7 @@ use super::Solver;
 use regex::Regex;
 use std::collections::HashMap;
 
+#[derive(PartialEq)]
 enum Operation {
     And,
     Or,
@@ -63,6 +64,15 @@ impl<'a> From<&'a str> for Solver2024_24<'a> {
     }
 }
 
+fn get_bit_index(variable: &str) -> Option<usize> {
+    let re = Regex::new(r"(x|y)(\d+)").unwrap();
+    re.captures(variable)?
+        .get(2)?
+        .as_str()
+        .parse::<usize>()
+        .ok()
+}
+
 impl<'a> Solver2024_24<'a> {
     fn get_value(&'a self, values: &mut HashMap<&'a str, bool>, variable: &'a str) -> bool {
         if let Some(value) = values.get(variable) {
@@ -97,15 +107,86 @@ impl<'a> Solver2024_24<'a> {
         }
         result
     }
+
+    fn get_xor_input_variables(&self, operation: Operation) -> HashMap<usize, &'a str> {
+        let mut result: HashMap<usize, &'a str> = HashMap::new();
+        for (variable, (rhs, op, lhs)) in self.assignments.iter() {
+            if operation != *op {
+                continue;
+            }
+            let rhs_index = get_bit_index(rhs);
+            let lhs_index = get_bit_index(lhs);
+            if rhs_index != lhs_index {
+                panic!("variables {} and {} are not the same", rhs, lhs);
+            } else if let Some(index) = rhs_index {
+                result.insert(index, variable);
+            }
+        }
+        result
+    }
+
+    fn is_nth_output_correct(
+        &self,
+        n: usize,
+        xor_input_variables: &HashMap<usize, &'a str>,
+    ) -> bool {
+        let &xor_input_variable = xor_input_variables.get(&n).unwrap();
+        if n == 0 {
+            return xor_input_variable == "z00";
+        }
+
+        let (rhs, op, lhs) = self
+            .assignments
+            .get(format!("z{:0>2}", n).as_str())
+            .unwrap();
+        *op == Operation::Xor && vec![*rhs, *lhs].contains(&xor_input_variable)
+    }
+
+    fn detect_tangled_variables(&self) -> Vec<String> {
+        let mut result = vec![];
+        let xor_input_variables = self.get_xor_input_variables(Operation::Xor);
+        for (&n, &variable) in xor_input_variables.iter() {
+            let output = format!("z{:0>2}", n);
+            if !self.is_nth_output_correct(n, &xor_input_variables) {
+                let (rhs, op, lhs) = self.assignments.get(output.as_str()).unwrap();
+                if *op != Operation::Xor {
+                    result.push(output.clone());
+                } else {
+                    result.push(variable.to_string());
+                    let (_, op, _) = self.assignments.get(lhs).unwrap();
+                    if *op == Operation::Or {
+                        result.push(rhs.to_string());
+                    } else {
+                        result.push(lhs.to_string());
+                    }
+                }
+            }
+        }
+
+        for (variable, (rhs, op, lhs)) in self.assignments.iter() {
+            if variable.starts_with("z") {
+                continue;
+            }
+            let rhs_index = get_bit_index(rhs);
+            let lhs_index = get_bit_index(lhs);
+            if *op == Operation::Xor && (rhs_index.is_none() || rhs_index != lhs_index) {
+                result.push(variable.to_string());
+            }
+        }
+
+        result
+    }
 }
 
-impl Solver<usize, usize> for Solver2024_24<'_> {
+impl Solver<usize, String> for Solver2024_24<'_> {
     fn solve_first_part(&self) -> usize {
         self.get_z_value()
     }
 
-    fn solve_second_part(&self) -> usize {
-        0
+    fn solve_second_part(&self) -> String {
+        let mut result = self.detect_tangled_variables();
+        result.sort();
+        result.join(",")
     }
 }
 
@@ -125,11 +206,5 @@ mod test {
     fn test_solve_first_part_with_example2() {
         let solver = Solver2024_24::from(EXAMPLE2);
         assert_eq!(solver.solve_first_part(), 2024);
-    }
-
-    #[test]
-    fn test_solve_second_part() {
-        let solver = Solver2024_24::from(EXAMPLE);
-        assert_eq!(solver.solve_second_part(), 0);
     }
 }
