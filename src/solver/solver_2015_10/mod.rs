ce6e292d1911b0b46cdb7f5d9fc47727db250dfb
Sertaç Karahoda <sertackarahoda@gmail.com>
2024-12-28 02:24:04 -0500

    :zap: Replace string with vector of numbers

--- src/solver/solver_2015_10/mod.rs
+++ src/solver/solver_2015_10/mod.rs
@@ -1,44 +1,60 @@
 use super::Solver;
 
-pub struct Solver2015_10<'a> {
-    input: &'a str,
+pub struct Solver2015_10 {
+    input: Vec<u8>,
 }
 
-impl Default for Solver2015_10<'_> {
+impl Default for Solver2015_10 {
     fn default() -> Self {
         Self::from(include_str!("input.txt"))
     }
 }
 
-impl<'a> From<&'a str> for Solver2015_10<'a> {
-    fn from(input: &'a str) -> Self {
-        Self { input }
+impl From<&str> for Solver2015_10 {
+    fn from(input: &str) -> Self {
+        Self {
+            input: input
+                .chars()
+                .map(|c| c.to_digit(10).unwrap() as u8)
+                .collect(),
+        }
     }
 }
 
-fn look_and_say(input: &str) -> String {
-    let mut result = String::new();
-    let mut chars = input.chars();
-    let mut current = chars.next().unwrap();
-    let mut count = 1;
-    for c in chars {
-        if c == current {
+fn look_and_say(input: &Vec<u8>) -> Vec<u8> {
+    let mut result = Vec::new();
+    result.reserve(input.len() * 2);
+    let mut iter = input.iter();
+    let mut current = *iter.next().unwrap();
+    let mut count: u8 = 1;
+    for &i in iter {
+        if i == current {
             count += 1;
         } else {
-            result.push_str(&count.to_string());
+            let mut v = count;
+            while v > 0 {
+                let n = (v % 10) as u8;
+                v /= 10;
+                result.push(n);
+            }
             result.push(current);
-            current = c;
+            current = i;
             count = 1;
         }
     }
-    result.push_str(&count.to_string());
+    let mut v = count;
+    while v > 0 {
+        let n = (v % 10) as u8;
+        v /= 10;
+        result.push(n);
+    }
     result.push(current);
     result
 }
 
-impl<'a> Solver2015_10<'a> {
+impl Solver2015_10 {
     fn apply_look_and_say_n_times(&self, n: usize) -> usize {
-        let mut result = self.input.to_string();
+        let mut result = self.input.clone();
         for _ in 0..n {
             result = look_and_say(&result);
         }
@@ -46,7 +62,7 @@ impl<'a> Solver2015_10<'a> {
     }
 }
 
-impl Solver<usize, usize> for Solver2015_10<'_> {
+impl Solver<usize, usize> for Solver2015_10 {
     fn solve_first_part(&self) -> usize {
         self.apply_look_and_say_n_times(40)
     }
@@ -62,26 +78,29 @@ mod tests {
 
     #[test]
     fn should_look_and_say_1() {
-        assert_eq!(look_and_say("1"), "11");
+        assert_eq!(look_and_say(&vec![1]), vec![1, 1]);
     }
 
     #[test]
     fn should_look_and_say_11() {
-        assert_eq!(look_and_say("11"), "21");
+        assert_eq!(look_and_say(&vec![1, 1]), vec![2, 1]);
     }
 
     #[test]
     fn should_look_and_say_21() {
-        assert_eq!(look_and_say("21"), "1211");
+        assert_eq!(look_and_say(&vec![2, 1]), vec![1, 2, 1, 1]);
     }
 
     #[test]
     fn should_look_and_say_1211() {
-        assert_eq!(look_and_say("1211"), "111221");
+        assert_eq!(look_and_say(&vec![1, 2, 1, 1]), vec![1, 1, 1, 2, 2, 1]);
     }
 
     #[test]
     fn should_look_and_say_111221() {
-        assert_eq!(look_and_say("111221"), "312211");
+        assert_eq!(
+            look_and_say(&vec![1, 1, 1, 2, 2, 1]),
+            vec![3, 1, 2, 2, 1, 1]
+        );
     }
 }
